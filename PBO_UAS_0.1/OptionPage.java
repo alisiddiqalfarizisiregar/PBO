b20ca7cee58d39710219aebe9e2ea38d0deba477
Ali Siddiq Al FArizi SIregar <114475419+alisiddiqalfarizisiregar@users.noreply.github.com>
2023-01-08 00:50:45 +0700

    Add files via upload

---
+++ PBO_UAS_0.1/OptionPage.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class OptionPage here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class OptionPage extends World
+{
+
+    /**
+     * Constructor for objects of class OptionPage.
+     * 
+     */
+    public OptionPage()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
